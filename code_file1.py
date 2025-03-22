8aed2242056d292fa308abd8d68f4e9738dc4a30
olijnykn <olijnyknicholas@gmail.com>
2025-03-22 18:42:53 -0400

    Update code_file1.py

--- code_file1.py
+++ code_file1.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import requests
+import pandas as pd
 
-st.write("Hello world")
+resp = requests.get('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20250101&end_date=20251231&station=8518750&product=hourly_height&datum=MLLW&time_zone=lst&units=metric&application=DataAPI_Sample&format=json')
+df = pd.DataFrame(resp.json()['data'])
+
+st.dataframe(df.head())
